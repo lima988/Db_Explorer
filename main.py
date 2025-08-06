a61d3cbd6c8e3102797a8e9037e44d917f625bb8
lima988 <limachakma17@gmail.com>
2025-08-06 12:26:26 +0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+# db.py now  inside of dialogs
+from dialogs.db import initialize_database
+from main_window import MainWindow
+
+if __name__ == "__main__":                                                                                                                                                                                                                                                                                
+    # database and necessary table  create 
+    initialize_database()
+
+    # main application start
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
